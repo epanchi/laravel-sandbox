048a9574a115368d697ad07e3874ccf59b652008
epanchi <edison.panchi@gmail.com>
2022-04-20 23:12:52 -0500

    additional CSS

--- resources/views/tw.blade.php
+++ resources/views/tw.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,19 +8,57 @@
     <link rel="stylesheet" href="css/app.css">
     <title>Document</title>
 </head>
+
 <body>
-    <h1 class="text-3xl font-bold underline"> Hello word</h1>
+    <div class="flex items-center p-6 max-w-sm mx-auto ">
+        <blockquote class="text-2xl font-semibold italic text-center text-slate-900">
+            When you look
+            <span
+                class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
+                <span class="relative text-white">annoyed</span>
+            </span>
+            all the time, people think that you're busy.
+        </blockquote>
+
+
+
+
+    </div>
+    <div class="flex items-center p-6 max-w-sm mx-auto ">
+        <div class="selection:bg-fuchsia-300 selection:text-fuchsia-900">
+            <p>
+                So I started to walk into the water. I won't lie to you boys, I was
+                terrified. But I pressed on, and as I made my way past the breakers
+                a strange calm came over me. I don't know if it was divine intervention
+                or the kinship of all living things but I tell you Jerry at that moment,
+                I <em>was</em> a marine biologist.
+            </p>
+        </div>
+<br>
+        <div class="bg-white dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+            <div>
+              <span class="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
+                <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><!-- ... --></svg>
+              </span>
+            </div>
+            <h3 class="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Writes Upside-Down</h3>
+            <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+              The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
+            </p>
+          </div>
+    </div>
+
 
     <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
         <div class="shrink-0">
-          <img class="h-12 w-12" src="/img/chat.svg" alt="ChitChat Logo">
-          
+            <img class="h-12 w-12" src="/img/chat.svg" alt="ChitChat Logo">
         </div>
         <div>
-          <div class="text-xl font-medium text-black">ChitChat</div>
-          <p class="text-slate-500">You have a new message!</p>
+            <div class="text-xl font-medium text-black">ChitChat</div>
+            <p class="text-slate-500">You have a new message!</p>
         </div>
-      </div>
+    </div>
 
 </body>
+
 </html>
